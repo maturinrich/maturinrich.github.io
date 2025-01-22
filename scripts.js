7ad19623be2ada16ae1354a3a7fa521b6299ba2b
maturinrich <maturinrichard96@gmail.com>
2025-01-22 22:32:44 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+// Dark Mode Toggle
+const darkModeToggle = document.querySelector('.btn-dark-mode');
 
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+/* CSS for Dark Mode */
+document.body.classList.add('dark-mode');
