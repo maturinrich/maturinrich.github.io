3fa9cfe771116dc3ec2cdbbab7c2cc58ed598f0f
maturinrich <maturinrichard96@gmail.com>
2025-01-22 22:36:41 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,3 @@ const darkModeToggle = document.querySelector('.btn-dark-mode');
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
-
-/* CSS for Dark Mode */
-document.body.classList.add('dark-mode');
